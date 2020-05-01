16b6fce910007a9adc802ed567b1ccf1cff746cb
SERHEI <serggarelov@gmail.com>
2020-05-01 19:16:51 +0300

    changes with the code

--- script.js
+++ script.js
@@ -10,64 +10,73 @@ $(document).ready(function () {
     var calcPower = calc.find('.calculator__power');
     var calcSpace = calc.find('.calculator__backspace');
 
+    function checkOperatorRepeat(expression) {
+        if (!expression) {
+            return true;
+        }
+
+            const expressionWithoutSpaces = expression.split(' ').join('');
+            if(expressionWithoutSpaces.length < 2) {
+                return true;
+            }
+
+            const length = expressionWithoutSpaces.length;
+            const lastSymbol = expressionWithoutSpaces[length - 1];
+            const prelastSymbol = expressionWithoutSpaces[length - 2];
 
 
-    // INIT CALC KEYS
-    calcKeys.each(function () {
-        var current = $(this).attr('value');
-        $(this).text(current);
+            return lastSymbol !== prelastSymbol || !(/[\/*\-+.]/.test(lastSymbol));
+        }
+
+
+        calcDisplay.on('keydown', (event) => {
+
+            const arithmeticPattern = /^[1-9\-][0-9\/*\-+. ]*$/;
+            const key = event.key;
+            if (key === 'Enter') {
+                calcEqual.click();
+                return true;
+            }
+            const value = `${calcDisplay.val()}${key}`;
+
+            return key === 'ArrowLeft' || key === 'ArrowRight' || key === 'Delete' || key === 'Backspace'
+                // должен работать и паттерн и проверка на повторку
+                || arithmeticPattern.test(value) && checkOperatorRepeat(value);
+        });
+
+        // INIT CALC KEYS
+        calcKeys.each(function () {
+            var current = $(this).attr('value');
+            $(this).text(current);
+        });
+
+        // ADD NUMBERS TO INPUT
+        calcButton.on('click', function () {
+            const value = calcDisplay.val() + $(this).attr('value');
+            if (checkOperatorRepeat(value)) {
+                calcDisplay.val(value);
+            }
+        });
+
+
+        // CLEAR INPUT
+        calcClear.on('click', function () {
+            calcDisplay.val('');
+        });
+
+        // SHOW RESULT
+        calcEqual.on('click', function () {
+            calcDisplay.val(eval(calcDisplay.val()));
+        });
+
+        // POWER BUTTON
+        calcPower.on('click', function () {
+            calcDisplay.val(Math.pow(calcDisplay.val(), 3));
+        });
+
+        // BACKSPACE BUTTON
+        calcSpace.on('click', function () {
+            calcDisplay.val(calcDisplay.val().substring(0, calcDisplay.val().length - 1));
+        });
     });
 
-    // ADD NUMBERS TO INPUT
-    calcButton.on('click', function () {
-        calcDisplay.val( calcDisplay.val() + $(this).attr('value') );
-    });
-
-
-    // CLEAR INPUT
-    calcClear.on('click', function () {
-        calcDisplay.val('');
-    });
-
-    // SHOW RESULT
-    calcEqual.on('click', function () {
-        calcDisplay.val( eval( calcDisplay.val() ));
-    });
-
-    // POWER BUTTON
-    calcPower.on('click', function () {
-        calcDisplay.val( Math.pow( calcDisplay.val(), 3 ) );
-    });
-
-    // BACKSPACE BUTTON
-    calcSpace.on('click', function () {
-        calcDisplay.val( calcDisplay.val().substring(0, calcDisplay.val().length-1) );
-    });
-
-    $('.calculator__button_plus').one('click',function(){
-
-        $('.calculator__button_plus').off('click');
-    });
-
-    $('.calculator__button_minus').one('click',function(){
-
-        $('.calculator__button_minus').off('click');
-    });
-
-    $('.calculator__button_multiplication').one('click',function(){
-
-        $('.calculator__button_multiplication').off('click');
-    });
-
-    $('.calculator__button_division').one('click',function(){
-
-        $('.calculator__button_division').off('click');
-    });
-
-    $('.calculator__button__point').one('click',function(){
-
-        $('.calculator__button__point').off('click');
-    });
-
-
-});
